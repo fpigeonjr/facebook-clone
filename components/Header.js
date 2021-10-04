0eb2876f059eb6a5dfec07abac1033eb8eeefbc3
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-04 15:19:57 -0500

    adds header

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import HeaderIcon from './HeaderIcon'
 import {
   BellIcon,
   ChatIcon,
@@ -16,7 +17,7 @@ import {
 
 export default function Header() {
   return (
-    <>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -29,14 +30,31 @@ export default function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            className="hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           />
         </div>
       </div>
       {/* Center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} active />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
       {/*  Right */}
-    </>
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* <Image /> */}
+        <p className="font-semibold pr-3 whitespace-nowrap">Frank Pigeon</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
+    </div>
   )
 }
