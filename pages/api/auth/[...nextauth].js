6fa8c64660c6a4e202cc820076d1552de0c88e6b
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-04 21:34:06 -0500

    adds Facebook auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,18 @@
 import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import FacebookProvider from 'next-auth/providers/facebook'
+import GitHubProvider from 'next-auth/providers/github'
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.Facebook({
+    FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
+    // GitHubProvider({
+    //   clientId: process.env.GITHUB_CLIENT_ID,
+    //   clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    // }),
     // ...add more providers here
   ],
 })
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import { getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />
   return (
     <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
@@ -18,4 +21,15 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // get user
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session,
+    },
+  }
 }
