8aa47f418fd642fcbf5e16f004bd65624162f462
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-08 14:32:52 -0500

    adds InputBox to Feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,12 +1,12 @@
 import Stories from './Stories'
+import InputBox from './InputBox'
 
 export default function Feed() {
   return (
     <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto ">
       <div className="mx-auto max-w-md md:max-w-lg">
-        {/* Stories */}
         <Stories />
-        {/* InputBox */}
+        <InputBox />
         {/* Posts */}
       </div>
     </div>
