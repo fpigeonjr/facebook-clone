5a01cdcb7bd76928f9c01f15d66bb8f47eed4e0a
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-12 17:53:17 -0500

    adds image upload feature

--- components/InputBox.js
+++ components/InputBox.js
@@ -9,21 +9,69 @@ import firebase from 'firebase/app'
 export default function InputBox() {
   const [session] = useSession()
   const inputRef = React.useRef(null)
+  const filepickerRef = React.useRef(null)
+  const [imageToPost, setImageToPost] = React.useState(null)
 
   function sendPost(e) {
     e.preventDefault()
 
     if (!inputRef.current.value) return
 
-    db.collection('posts').add({
-      message: inputRef.current.value,
-      name: session.user.name,
-      email: session.user.email,
-      image: session.user.image,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    })
+    db.collection('posts')
+      .add({
+        message: inputRef.current.value,
+        name: session.user.name,
+        email: session.user.email,
+        image: session.user.image,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then((doc) => {
+        if (imageToPost) {
+          const uploadTask = storage
+            .ref(`posts/${doc.id}`)
+            .putString(imageToPost, 'data_url')
+
+          removeImage()
+
+          uploadTask.on(
+            'state_change',
+            null,
+            (error) => console.error(error),
+            () => {
+              //when upload completes
+              storage
+                .ref('posts')
+                .child(doc.id)
+                .getDownloadURL()
+                .then((url) =>
+                  db.collection('posts').doc(doc.id).set(
+                    {
+                      postImage: url,
+                    },
+                    { merge: true }
+                  )
+                )
+            }
+          )
+        }
+      })
 
     inputRef.current.value = ''
+  }
+
+  function addImageToPost(e) {
+    const reader = new FileReader()
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0])
+    }
+
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result)
+    }
+  }
+
+  function removeImage() {
+    setImageToPost(null)
   }
 
   return (
@@ -48,15 +96,38 @@ export default function InputBox() {
             Submit
           </button>
         </form>
+
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className="flex flex-col filter hover:brightness-110 transition duration-150  transform hover:scale-105 cursor-pointer"
+          >
+            <img
+              className="h-10 object-contain"
+              src={imageToPost}
+              alt="image from user"
+            />
+            <p className="text-sm text-red-500 text-center">Remove</p>
+          </div>
+        )}
       </div>
       <div className="flex justify-evenly p-3 border-t">
         <div className="inputIcon">
           <VideoCameraIcon className="h-7 text-red-500 " />
           <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
         </div>
-        <div className="inputIcon">
+        <div
+          onClick={() => filepickerRef.current.click()}
+          className="inputIcon"
+        >
           <CameraIcon className="h-7 text-green-400" />
           <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+          <input
+            ref={filepickerRef}
+            type="file"
+            onChange={addImageToPost}
+            hidden
+          />
         </div>
         <div className="inputIcon">
           <EmojiHappyIcon className="h-7 text-yellow-300 " />
