b0e4ed4234ecfc093c29511e4002daa840b80fc7
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-15 10:33:24 -0500

    adds server-side rendering of posts

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ import Sidebar from '../components/Sidebar'
 import Login from '../components/Login'
 import Feed from '../components/Feed'
 import Widgets from '../components/Widgets'
+import { db } from '../firebase'
 
-export default function Home({ session }) {
+export default function Home({ session, posts }) {
   if (!session) return <Login />
   return (
     <div className="h-screen bg-gray-100 overflow-hidden">
@@ -17,7 +18,7 @@ export default function Home({ session }) {
       <Header />
       <main className="flex">
         <Sidebar />
-        <Feed />
+        <Feed posts={posts} />
         <Widgets />
       </main>
     </div>
@@ -28,9 +29,18 @@ export async function getServerSideProps(context) {
   // get user
   const session = await getSession(context)
 
+  const posts = await db.collection('posts').orderBy('timestamp', 'desc').get()
+
+  const docs = posts.docs.map((post) => ({
+    id: post.id,
+    ...post.data(),
+    timestamp: null,
+  }))
+
   return {
     props: {
       session,
+      posts: docs,
     },
   }
 }
