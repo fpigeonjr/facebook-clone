bb14ccf2a2f986c6673e918c812a26a969281942
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-15 10:40:04 -0500

    adds loading state

--- components/Post.js
+++ components/Post.js
@@ -23,9 +23,13 @@ export default function Post({
           />
           <div>
             <p className="font-medium">{name}</p>
-            <p className="text-sm text-gray-400">
-              {new Date(timestamp?.toDate()).toLocaleString()}
-            </p>
+            {timestamp ? (
+              <p className="text-sm text-gray-400">
+                {new Date(timestamp?.toDate()).toLocaleString()}
+              </p>
+            ) : (
+              <p className="text-sm text-gray-400">Loading</p>
+            )}
           </div>
         </div>
         <p className="pt-4">{message}</p>
